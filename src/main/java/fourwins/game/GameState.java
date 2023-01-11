1f26efa0644a3c0d1211615020caf7e9b4a02c27
dotDani <64549494+Day-Hawk@users.noreply.github.com>
2023-01-11 13:31:07 +0100

    dev: Added doc

--- src/main/java/fourwins/game/GameState.java
+++ src/main/java/fourwins/game/GameState.java
@@ -4,7 +4,18 @@ package fourwins.game;
  * Expresses the state of a game.
  */
 public enum GameState {
-  PREPARING, //When the game is being created [default value].
-  RUNNING, //When the game is on.
-  END; //When the game is finished and a winner is determined.
+  /**
+   * When the game is being created [default value].
+   */
+  PREPARING,
+
+  /**
+   * When the game is on.
+   */
+  RUNNING,
+
+  /**
+   * //When the game is finished and a winner is determined.
+   */
+  END;
 }
